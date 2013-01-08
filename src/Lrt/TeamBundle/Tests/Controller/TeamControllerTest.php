5c215a69fd0341bf7981de5cfb0c4acd68b05e8d
Alex <longchamp-roller-team@laposte.net>
2013-01-08 19:10:12 +0100

    Tests

--- src/Lrt/TeamBundle/Tests/Controller/TeamControllerTest.php
+++ src/Lrt/TeamBundle/Tests/Controller/TeamControllerTest.php
@@ -15,7 +15,7 @@ class TeamControllerTest extends WebTestCase
         // Create a new entry in the database
         $crawler = $client->request('GET', '/team/');
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
+        $crawler = $client->click($crawler->selectLink('Ajouter une équipe')->link());
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
@@ -31,21 +31,21 @@ class TeamControllerTest extends WebTestCase
         // Edit the entity
         $crawler = $client->click($crawler->selectLink('Edit')->link());
 
-        $form = $crawler->selectButton('Edit')->form(array(
-            'lrt_teambundle_teamtype[name]'  => 'Foo',
+        $form = $crawler->selectButton('Valider')->form(array(
+            'lrt_teambundle_teamtype[name]'  => 'Test',
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check the element contains an attribute with value equals "Foo"
-        $this->assertTrue($crawler->filter('[value="Foo"]')->count() > 0);
+        $this->assertTrue($crawler->filter('[value="Test"]')->count() > 0);
 
         // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
+        $client->submit($crawler->selectButton('Supprimer')->form());
         $crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $this->assertNotRegExp('/Test/', $client->getResponse()->getContent());
     }
 }
