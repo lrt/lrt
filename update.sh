996db6ca5880eea5f8eed61f57a23e2ea6b1299b
Alex <longchamp-roller-team@laposte.net>
2013-03-25 18:42:42 +0100

    file update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 git stash
-git pull
+git pull origin develop
 php-cli app/console cache:clear --env=preprod
 php-cli app/console doctrine:schema:update --force --env=preprod
 php-cli app/console assets:install
